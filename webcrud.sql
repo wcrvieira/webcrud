f1d1bc28c8f46ed3eff524baf6aabb5faa264b99
wcrvieira <wagner.vieira@etec.sp.gov.br>
2022-10-19 15:18:30 -0300

    Campo e-mail inserido

--- webcrud.sql
+++ webcrud.sql
@@ -4,21 +4,25 @@ USE webcrud;
 
 CREATE TABLE customers (
   id int(11) NOT NULL,
-  name varchar(255) NOT NULL,
+  name varchar(80) NOT NULL,
   cpf_cnpj varchar(14) NOT NULL,
   birthdate date NOT NULL,
-  address varchar(255) NOT NULL,
-  hood varchar(100) NOT NULL,
-  zip_code int(8) NOT NULL,
-  city varchar(100) NOT NULL,
-  state varchar(100) NOT NULL,
-  phone int(13) NOT NULL,
-  mobile int(13) NOT NULL,
-  ie int(11) NOT NULL,
+  address varchar(100) NOT NULL,
+  hood varchar(50) NOT NULL,
+  zip_code varchar(10) DEFAULT NULL,
+  city varchar(60) NOT NULL,
+  state varchar(2) NOT NULL,
+  phone varchar(20) DEFAULT NULL,
+  mobile varchar(20) DEFAULT NULL,
+  ie varchar(12) DEFAULT NULL,
   created datetime NOT NULL,
-  modified datetime NOT NULL
-);
+  modified datetime NOT NULL,
+  email varchar(60) DEFAULT NULL
+) ;
 
+--
+-- Índices para tabela customers
+--
 ALTER TABLE customers
   ADD PRIMARY KEY (id);
   
